dced75a588d84267ced3898c0716eb71d7a15ff8
Aditia Pratama <mhdaditiapratama268@gmail.com>
2026-04-20 19:51:37 +0700

    fix: contact-form after submit

--- script.js
+++ script.js
@@ -44,6 +44,9 @@ window.dispatchEvent(new Event('scroll'));
 document.getElementById('contactForm').addEventListener('submit', function (e) {
   e.preventDefault();
   alert('(Demo) Message sent!');
+  document.getElementById('name-input').value = "";
+  document.getElementById('email-input').value = "";
+  document.getElementById('msg-input').value = "";
 });
 
 
