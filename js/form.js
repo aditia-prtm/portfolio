cbc596c8438f13be077c141546fb7e445d536fbc
Aditia Pratama <mhdaditiapratama268@gmail.com>
2026-04-23 22:31:58 +0700

    refactor: split css code

--- js/form.js
+++ js/form.js
@@ -1,6 +1,6 @@
 document.getElementById('contactForm').addEventListener('submit', function (e) {
   e.preventDefault();
-  alert('(Demo) Message sent!');
+  alert('Message sent!');
   document.getElementById('name-input').value = "";
   document.getElementById('email-input').value = "";
   document.getElementById('msg-input').value = "";
